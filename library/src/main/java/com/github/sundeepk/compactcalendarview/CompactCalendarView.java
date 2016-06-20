3eb9b9b95ea15f578368b5e8327336fffdebe1a0
SundeepK <SundeepK@users.noreply.github.com>
2016-06-20 16:45:25 +0100

    Bulk experimental changes.

--- library/src/main/java/com/github/sundeepk/compactcalendarview/CompactCalendarView.java
+++ library/src/main/java/com/github/sundeepk/compactcalendarview/CompactCalendarView.java
@@ -55,7 +55,7 @@ public class CompactCalendarView extends View {
 
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            if(shouldScroll) {
+            if (shouldScroll) {
                 compactCalendarController.onScroll(e1, e2, distanceX, distanceY);
                 invalidate();
             }
@@ -74,7 +74,7 @@ public class CompactCalendarView extends View {
     public CompactCalendarView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         compactCalendarController = new CompactCalendarController(new Paint(), new OverScroller(getContext()),
-                new Rect(), attrs, getContext(),  Color.argb(255, 233, 84, 81),
+                new Rect(), attrs, getContext(), Color.argb(255, 233, 84, 81),
                 Color.argb(255, 64, 64, 64), Color.argb(255, 219, 219, 219), VelocityTracker.obtain(),
                 Color.argb(255, 100, 68, 65));
         gestureDetector = new GestureDetectorCompat(getContext(), gestureListener);
@@ -84,7 +84,7 @@ public class CompactCalendarView extends View {
     /*
     Use a custom locale for compact calendar.
      */
-    public void setLocale(Locale locale){
+    public void setLocale(Locale locale) {
         compactCalendarController.setLocale(locale);
         invalidate();
     }
@@ -94,7 +94,7 @@ public class CompactCalendarView extends View {
     The default is to use the default locale and to abbreviate the day names to one character.
     Setting this to true will displace the short weekday string provided by java.
      */
-    public void setUseThreeLetterAbbreviation(boolean useThreeLetterAbbreviation){
+    public void setUseThreeLetterAbbreviation(boolean useThreeLetterAbbreviation) {
         compactCalendarController.setUseWeekDayAbbreviation(useThreeLetterAbbreviation);
         invalidate();
     }
@@ -108,7 +108,7 @@ public class CompactCalendarView extends View {
     Sets the name for each day of the week. No attempt is made to adjust width or text size based on the length of each day name.
     Works best with 3-4 characters for each day.
      */
-    public void setDayColumnNames(String[] dayColumnNames){
+    public void setDayColumnNames(String[] dayColumnNames) {
         compactCalendarController.setDayColumnNames(dayColumnNames);
     }
 
@@ -127,50 +127,52 @@ public class CompactCalendarView extends View {
         invalidate();
     }
 
-    public int getHeightPerDay(){
+    public int getHeightPerDay() {
         return compactCalendarController.getHeightPerDay();
     }
 
-    public void setListener(CompactCalendarViewListener listener){
+    public void setListener(CompactCalendarViewListener listener) {
         compactCalendarController.setListener(listener);
     }
 
-    public Date getFirstDayOfCurrentMonth(){
+    public Date getFirstDayOfCurrentMonth() {
         return compactCalendarController.getFirstDayOfCurrentMonth();
     }
 
-    public void setCurrentDate(Date dateTimeMonth){
+    public void setCurrentDate(Date dateTimeMonth) {
         compactCalendarController.setCurrentDate(dateTimeMonth);
         invalidate();
     }
 
-    public int getWeekNumberForCurrentMonth(){
+    public int getWeekNumberForCurrentMonth() {
         return compactCalendarController.getWeekNumberForCurrentMonth();
     }
 
-    public void setShouldDrawDaysHeader(boolean shouldDrawDaysHeader){
+    public void setShouldDrawDaysHeader(boolean shouldDrawDaysHeader) {
         compactCalendarController.setShouldDrawDaysHeader(shouldDrawDaysHeader);
     }
 
     /**
      * see {@link #addEvent(Event, boolean)} when adding single events
      * or {@link #addEvents(java.util.List)}  when adding multiple events
+     *
      * @param event
      */
     @Deprecated
-    public void addEvent(Event event){
+    public void addEvent(Event event) {
         addEvent(event, false);
     }
 
     /**
-     *  Adds an event to be drawn as an indicator in the calendar.
-     *  If adding multiple events see {@link #addEvents(List)}} method.
-     * @param event to be added to the calendar
+     * Adds an event to be drawn as an indicator in the calendar.
+     * If adding multiple events see {@link #addEvents(List)}} method.
+     *
+     * @param event            to be added to the calendar
      * @param shouldInvalidate true if the view should invalidate
      */
-    public void addEvent(Event event, boolean shouldInvalidate){
+    public void addEvent(Event event, boolean shouldInvalidate) {
         compactCalendarController.addEvent(event);
-        if(shouldInvalidate){
+        if (shouldInvalidate) {
             invalidate();
         }
     }
@@ -178,48 +180,52 @@ public class CompactCalendarView extends View {
     /**
      * Adds multiple events to the calendar and invalidates the view once all events are added.
      */
-    public void addEvents(List<Event> events){
+    public void addEvents(List<Event> events) {
         compactCalendarController.addEvents(events);
         invalidate();
     }
 
     /**
      * Fetches the events for the date passed in
+     *
      * @param date
      * @return
      */
-    public List<Event> getEvents(Date date){
+    public List<Event> getEvents(Date date) {
         return compactCalendarController.getCalendarEventsFor(date);
     }
 
     /**
      * Fetches the events for the epochMillis passed in
+     *
      * @param epochMillis
      * @return
      */
-    public List<Event> getEvents(long epochMillis){
+    public List<Event> getEvents(long epochMillis) {
         return compactCalendarController.getCalendarEventsFor(epochMillis);
     }
 
     /**
      * Remove the event associated with the Date passed in
+     *
      * @param date
      */
-    public void removeEvents(Date date){
+    public void removeEvents(Date date) {
         compactCalendarController.removeEventsByDate(date);
     }
 
-    public void removeEvents(long epochMillis){
+    public void removeEvents(long epochMillis) {
         compactCalendarController.removeEventByEpochMillis(epochMillis);
     }
 
     /**
      * see {@link #removeEvent(Event, boolean)} when removing single events
      * or {@link #removeEvents(java.util.List)} (java.util.List)}  when removing multiple events
+     *
      * @param event
      */
     @Deprecated
-    public void removeEvent(Event event){
+    public void removeEvent(Event event) {
         removeEvent(event, false);
     }
 
@@ -227,12 +233,12 @@ public class CompactCalendarView extends View {
      * Removes an event from the calendar.
      * If removing multiple events see {@link #removeEvents(List)}
      *
-     * @param event event to remove from the calendar
+     * @param event            event to remove from the calendar
      * @param shouldInvalidate true if the view should invalidate
      */
-    public void removeEvent(Event event, boolean shouldInvalidate){
+    public void removeEvent(Event event, boolean shouldInvalidate) {
         compactCalendarController.removeEvent(event);
-        if(shouldInvalidate){
+        if (shouldInvalidate) {
             invalidate();
         }
     }
@@ -240,7 +246,7 @@ public class CompactCalendarView extends View {
     /**
      * Adds multiple events to the calendar and invalidates the view once all events are added.
      */
-    public void removeEvents(List<Event> events){
+    public void removeEvents(List<Event> events) {
         compactCalendarController.removeEvents(events);
         invalidate();
     }
@@ -259,32 +265,32 @@ public class CompactCalendarView extends View {
         }
     }
 
-    public void showCalendar(){
+    public void showCalendar() {
         checkTargetHeight();
         animationHandler.openCalendar();
     }
 
-    public void hideCalendar(){
+    public void hideCalendar() {
         checkTargetHeight();
         animationHandler.closeCalendar();
     }
 
-    public void showCalendarWithAnimation(){
+    public void showCalendarWithAnimation() {
         checkTargetHeight();
         animationHandler.openCalendarWithAnimation();
     }
 
-    public void hideCalendarWithAnimation(){
+    public void hideCalendarWithAnimation() {
         checkTargetHeight();
         animationHandler.closeCalendarWithAnimation();
     }
 
-    public void showNextMonth(){
+    public void showNextMonth() {
         compactCalendarController.showNextMonth();
         invalidate();
     }
 
-    public void showPreviousMonth(){
+    public void showPreviousMonth() {
         compactCalendarController.showPreviousMonth();
         invalidate();
     }
@@ -294,7 +300,7 @@ public class CompactCalendarView extends View {
         super.onMeasure(parentWidth, parentHeight);
         int width = MeasureSpec.getSize(parentWidth);
         int height = MeasureSpec.getSize(parentHeight);
-        if(width > 0 && height > 0) {
+        if (width > 0 && height > 0) {
             compactCalendarController.onMeasure(width, height, getPaddingRight(), getPaddingLeft());
         }
         setMeasuredDimension(width, height);
@@ -308,18 +314,26 @@ public class CompactCalendarView extends View {
     @Override
     public void computeScroll() {
         super.computeScroll();
-        if(compactCalendarController.computeScroll()){
+        if (compactCalendarController.computeScroll()) {
             invalidate();
         }
     }
 
-    public void shouldScrollMonth(boolean shouldDisableScroll){
+    public void shouldScrollMonth(boolean shouldDisableScroll) {
         this.shouldScroll = shouldDisableScroll;
     }
 
     public boolean onTouchEvent(MotionEvent event) {
         compactCalendarController.onTouch(event);
         invalidate();
+
+        // prevent parent container from processing ACTION_MOVE events (scroll inside ViewPager issue #82)
+//        if (event.getAction() == MotionEvent.ACTION_MOVE) {
+//            getParent().requestDisallowInterceptTouchEvent(true);
+//        } else if (event.getAction() == MotionEvent.ACTION_CANCEL) {
+//            getParent().requestDisallowInterceptTouchEvent(false);
+//        }
+
         // always allow gestureDetector to detect onSingleTap and scroll events
         return gestureDetector.onTouchEvent(event);
     }
---
+++ sample/src/main/java/sundeepk/github/com/sample/Tab1.java
@@ -0,0 +1,17 @@
+package sundeepk.github.com.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.main_tab,container,false);
+        return v;
+    }
+}
